db32907be2e85419f322dfdce4d668238c2f2a63
lexmic <ml.workbase@gmail.com>
2017-03-08 19:15:30 +0700

    Add function Sum and case

--- TheLuckyTickets.c
+++ TheLuckyTickets.c
@@ -2,10 +2,28 @@
 #include <ctype.h>
 #include <stdlib.h>
 
+void Sum(int Ticket[6])
+{
+    int i, Lsum = 0, Rsum = 0, substract;
+
+    for (int i = 0; i < 3; ++i) {
+        Lsum = Lsum + Ticket[i];
+        Rsum = Rsum + Ticket[i+3];
+    }
+
+    if (Lsum == Rsum) {
+        printf("Ваш билет оказался счастливым!\n");
+    }
+}
+
+int Search();
+{
+
+}
 
 int main() {
-    char a;
-    int i = 0, ticket[6], ch;
+    char a, y;
+    int i = 0, ticket[6], ch, w;
     
     printf("Enter a ticket: ");
     while ((a = getchar()) != '\n') {
@@ -18,10 +36,25 @@ int main() {
             exit(1);
         }
     }
-    for (i = 0;i < 6;i++) {
+    /*for (i = 0;i < 6;i++) {
         printf("%d", ticket[i]);
     }
-    printf("\n");
+    printf("\n");*/
+
+    Sum(ticket);
+
+    switch(y){
+        case 'Да':
+        {
+            Search(ticket);
+        }
+        case 'Нет':
+        {
+            printf("Извини, но я всё равно выведу тебе: %d\n", );
+            return 0;
+        }
+    }
+
     return 0;
 }
 
