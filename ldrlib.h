b42cb2975ed2eea6d2865905f1c0159e32f7ade0
Satan Love <panmihaurkin@icloud.com>
2017-03-19 19:55:09 +0700

    Prototypes for current loading functions

---
+++ ldrlib.h
@@ -0,0 +1,6 @@
+int *load_memory_init(size_t init_elems);
+int program_weight_transform(size_t init_program_weight);
+void pred_loadbar_init(size_t program_weight);
+void part_process_loadbar(size_t operation_weight);
+void whole_process_loadbar(size_t ldr_program_weight);
+
